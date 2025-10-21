bfd9a6b93cc26ab0486c4fa5dd70cb947cf217f1
joaodinizaraujo <joaovictorclassroom@gmail.com>
2025-10-21 09:10:58 -0300

    add category attribute

--- database/schema.sql
+++ database/schema.sql
@@ -38,6 +38,7 @@ CREATE TABLE tb_ideas (
     id VARCHAR(255) PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
+    category VARCHAR(255) NOT NULL,
     owner_id VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
